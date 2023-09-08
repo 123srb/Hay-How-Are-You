9021adfbd2193aa0ea9f74be7bea20c267061b28
Steven Burgess <stevenryanburgess@gmail.com>
2023-09-07 22:01:31 -0700

    Adding some error checking for the form builder
    also more features

--- fields.py
+++ fields.py
@@ -15,6 +15,7 @@ cursor.execute( """CREATE TABLE IF NOT EXISTS entries (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     entry TEXT NOT NULL,
     type TEXT NOT NULL,
+    variable_type TEXT NOT NULL,
     default_type TEXT,
     choices TEXT
 );"""
@@ -24,7 +25,8 @@ conn.close()
 # Define a form for editing and adding rows
 class EntryForm(FlaskForm):
     entry = StringField('Entry', validators=[DataRequired()])
-    type = SelectField('Type', choices=[('SelectField', 'SelectField'), ('RadioField', 'RadioField'), ('StringField', 'StringField'), ('TextAreaField', 'TextAreaField'), ('Boolean', 'Boolean')])
+    type = SelectField('Type', choices=[('StringField', 'StringField'), ('TextAreaField', 'TextAreaField'), ('Boolean', 'Boolean'), ('SelectField', 'SelectField'), ('RadioField', 'RadioField')])
+    variable_type = SelectField('Default Value',choices=[('Binary', 'Yes or No'), ('Integer', 'Numeric'), ('String', 'Text')] ) 
     default_type = SelectField('Default Value',choices=[('Empty', 'Empty'), ('Default Value', 'Default Value'), ('Load Previous Value', 'Load Previous Value')] )
     choices = StringField('Choices')
 
@@ -61,8 +63,8 @@ def edit(id):
     if form.validate_on_submit():
         # Update the entry in the database
         data = form.data
-        cursor.execute("UPDATE entries SET entry=?, type=?, default_type=?, choices=? WHERE id=?", (
-            data['entry'], data['type'], data['default_type'], data['choices'], id))
+        cursor.execute("UPDATE entries SET entry=?, type=?, variable_type=?, default_type=?, choices=? WHERE id=?", (
+            data['entry'], data['type'], data['variable_type'], data['default_type'], data['choices'], id))
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
@@ -74,18 +76,31 @@ def edit(id):
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = EntryForm()
-
+    message = 'For choices, please remember to use a comma seperated list'
     if form.validate_on_submit():
-        data = form.data
+        data = form.data 
+        print('--------')
+        print(data['type'])
+        if data['type'] in ['SelectField','RadioField']:
+            choices_list = data['choices'].split(',')
+            choices_list = [x for x in choices_list if x] 
+            print('**********************')
+            print(choices_list)
+            print(len(choices_list))
+            if len(choices_list) <= 1:
+                print('cccccccccc')
+                message = 'It looks like you do not have multiple values in your choices, please seperate each value with a comma'
+                return render_template('add.html', form=form, result_message=message)
+
         conn = sqlite3.connect(DATABASE)
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO entries (entry, type, default_type, choices) VALUES (?, ?, ?, ?)",
-                       (data['entry'], data['type'], data['default_type'], data['choices']))
+        cursor.execute("INSERT INTO entries (entry, type, variable_type, default_type, choices) VALUES (?, ?, ?, ?, ?)",
+                       (data['entry'], data['type'],data['variable_type'], data['default_type'], data['choices']))
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
 
-    return render_template('add.html', form=form)
+    return render_template('add.html', form=form, result_message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
